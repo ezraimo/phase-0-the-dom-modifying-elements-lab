6ec5f0ddec19766487753399e09d51a476695651
EZRA KIPRUTO SEREM <ezraserem4038@gmail.com>
2025-02-28 09:10:08 +0300

    added the dom

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Remove the <main> with id 'main'
+document.querySelector('main#main').remove()
+
+// Create new header element
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Serem is the champion'
+
+// Append the newHeader to the body
+document.body.appendChild(newHeader)
